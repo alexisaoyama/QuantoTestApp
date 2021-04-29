a38d02f21f7c64f6894d54afd3ed8a06e22650e2
naokisosa <naokisosa1997@gmail.com>
2021-04-28 23:58:00 -0400

    importar React al index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
