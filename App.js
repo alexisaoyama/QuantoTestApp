cb42221c55e9455858d504aa61866a4c976bf3cf
naokisosa <naokisosa1997@gmail.com>
2021-04-28 23:57:36 -0400

    wip - containers de lista y detalles

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import UsersList from './containers/UsersList/UsersList';
 const App = () => {
   return (
     <View>
-      <Text>Hola mundo</Text>
+      <Text>Hola soy App</Text>
+      <UsersList />
     </View>
   );
 };
---
+++ containers/UserDetails/UserDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UserDetails = () => {
+  return (
+    <View>
+      <Text>Hola soy User Details</Text>
+    </View>
+  );
+};
+
+export default UserDetails;
